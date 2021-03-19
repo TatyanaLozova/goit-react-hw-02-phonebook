eba82c61e2f56a09063be1a5721f7d2266ea621f
Tatyana Loza <fly220@ukr.net>
2021-03-19 21:56:13 +0300

    оформление стилей

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,15 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import s from './ContactList.module.css'
+
 
 
 const ContactList = ({ contacts, onDeleteContact }) => {
   return (
-    <ul>
+    <ul >
       {contacts.map(({ id, name, number }) => (
-        <li key={id}>
+        <li className={s.contact} key={id}>
               <p>{name}: </p>
               <p>{number}</p>
-          <button
+          <button className={s.btn}
             type="button"
             onClick={() => onDeleteContact(id)}
           >
